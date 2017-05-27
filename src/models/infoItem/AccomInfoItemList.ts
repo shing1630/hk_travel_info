93f5e2c036ef36b7be75105c946361581d1b4b26
shing1630 <chan1630@hotmail.com>
2017-05-27 10:31:22 +0800

    phase one;

--- src/models/infoItem/AccomInfoItemList.ts
+++ src/models/infoItem/AccomInfoItemList.ts
@@ -24,9 +24,9 @@ export class AccomInfoItemList {
         accomInfoItem.info1DescZH=	'香港的士，即香港的的士，「的士」一字是從計程車的英語「Taxi」音譯出來的。乘客上車後指明目的地，司機直接駕駛前往，是方便快捷的公共交通工具。車費根據車程及等候時間而定，而存放行李、電話召喚的士及使用收費道路等則需繳付附加費用。香港各區設有不少的士站供乘客等候的士，而部分停車禁區亦設有一些的士專用的上落客位置。'
         accomInfoItem.info1Pic=	'https://vgy.me/GMohI1.jpeg'
         accomInfoItem.info2Flag=	true
-        accomInfoItem.info2DescEN=	''
-        accomInfoItem.info2DescCN=	''
-        accomInfoItem.info2DescZH=	''
+        accomInfoItem.info2DescEN=	'Taxis of Hong Kong provide a taxi system. Although a few taxis are independently owned and operated, the vast majority are owned by 17 independent taxi companies that rent out taxis on a shift basis to 40,000 self-employed drivers.'
+        accomInfoItem.info2DescCN=	'香港的士，即香港的出租车，“的士”一字是从出租车的英语“Taxi”音译出来的。乘客上车后指明目的地，司机直接驾驶前往，是方便快捷的公共交通工具。车费根据车程及等候时间而定，而存放行李、电话召唤出租车及使用收费道路等则需缴付附加费用。香港各区设有不少出租车站供乘客等候出租车，而部分停车禁区亦设有一些出租车专用的上落客位置。'
+        accomInfoItem.info2DescZH=	'香港的士，即香港的的士，「的士」一字是從計程車的英語「Taxi」音譯出來的。乘客上車後指明目的地，司機直接駕駛前往，是方便快捷的公共交通工具。車費根據車程及等候時間而定，而存放行李、電話召喚的士及使用收費道路等則需繳付附加費用。香港各區設有不少的士站供乘客等候的士，而部分停車禁區亦設有一些的士專用的上落客位置。'
         accomInfoItem.info2Pic=	'https://vgy.me/FenAPG.jpeg'
         accomInfoItem.info3Flag=	false
         accomInfoItem.info3DescEN=	''
--- src/models/infoItem/InfoItem.ts
+++ src/models/infoItem/InfoItem.ts
@@ -14,46 +14,46 @@ export class InfoItem {
     infoLogo: string;
 
     info1Flag: boolean;
-    info1Pic: string;
     info1DescEN: string;
     info1DescCN: string;
     info1DescZH: string;
+    info1Pic: string;
 
     info2Flag: boolean;
-    info2Pic: string;
     info2DescEN: string;
     info2DescCN: string;
     info2DescZH: string;
+    info2Pic: string;
 
     info3Flag: boolean;
-    info3Pic: string;
     info3DescEN: string;
     info3DescCN: string;
     info3DescZH: string;
+    info3Pic: string;
 
     info4Flag: boolean;
-    info4Pic: string;
     info4DescEN: string;
     info4DescCN: string;
     info4DescZH: string;
+    info4Pic: string;
 
     info5Flag: boolean;
-    info5Pic: string;
     info5DescEN: string;
     info5DescCN: string;
     info5DescZH: string;
+    info5Pic: string;
 
     info6Flag: boolean;
-    info6Pic: string;
     info6DescEN: string;
     info6DescCN: string;
     info6DescZH: string;
+    info6Pic: string;
 
     info7Flag: boolean;
-    info7Pic: string;
     info7DescEN: string;
     info7DescCN: string;
     info7DescZH: string;
+    info7Pic: string;
 
     infoAppFlag: boolean;
     appItemIdlist: Array<String> = new Array();
